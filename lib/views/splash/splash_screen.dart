ee6d6f58a227f0ecb589c92004f57e155389db53
Asif-Rezan <asifrezan@gmail.com>
2025-02-06 14:18:05 +0600

    add loading widged

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:it_way_bd_task/config/components/loading_widget.dart';
 import '../../config/routes/routes_name.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,12 +13,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, RoutesName.homeScreen);
-            },
-            child: Text('Home')),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RoutesName.homeScreen);
+                  },
+                  child: Text('Home')),
+            ],
+          ),
+        ),
       ),
     );
   }
